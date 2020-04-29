b9598afe64a2076b0389e474512f860302b4375c
Will <w.alexander.johnson@gmail.com>
2020-04-28 23:18:34 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ApplicationRecord
+    # validates that there is a username and that there isnt a match in the database regardless of casing, as well as min and max characther limits
+    validates :username, presence: true, uniqueness: { case_sensitive: false}, length: { minimum: 3, maximum: 25}
+    
+    # constant to store regex
+    VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      # validates that there is an email and there isn't a match regardless of case and a maximum length & correct email format with regex
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false}, 
+                        length: { maximum: 105 }, 
+                        format: { with: VALID_EMAIL_REGEX }
 
 end
